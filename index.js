bd2400dc1deb79cafbfe8d8be8a1271198696bc5
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-08-04 16:57:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nameToAttributes (array)
 function attributesToPhrase(array)
 {
   return array.map(function(item){
-    return `${}`;
+    return `${name} is from ${hometown}`;
   });
 }
 
